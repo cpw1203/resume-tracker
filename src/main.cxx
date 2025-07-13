9018aa324b17c134af04a763dcab30f9bcfd9a06
Christopher Waschenko <cwaschenk03@gmail.com>
2025-07-13 18:32:51 -0400

    using ftxui

--- src/main.cxx
+++ src/main.cxx
@@ -1,21 +1,48 @@
 #include <iostream>
-#include <resume-application.hxx>
+#include <application.hxx>
 #include <ftxui/component/component.hpp>
+#include <ftxui/component/component_options.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 
 using namespace ftxui;
 
-
 int main(int argc, char** argv)
 {
-  auto win = ScreenInteractive::Fullscreen();
-  auto box = vbox({ text("Hello") });
-  Component renderer = Renderer([]{
+  auto screen = ScreenInteractive::Fullscreen();
+  auto exit_btn = Button("Exit", [&]{ screen.Exit(); }, ButtonOption::Animated());
+  std::vector<Application> apps;
+  std::vector<Element> app_elements;
+  auto layout = Renderer([&](){
+    for(auto& app : apps){
+      char buffer[80];
+      std::tm* tm_info = std::localtime(&app.date_applied);
+      std::strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", tm_info);
+      app_elements.push_back(
+          flexbox({
+            text(app.title) | bold | center,
+            separatorHSelector(0,5000,Color::Red, Color::White),
+            text(app.company) | bold | color(Color::Blue) | center,
+            separatorHSelector(0,5000,Color::Red, Color::White),
+            text(buffer) | bold,
+            }) | border
+          );
+      }
       return vbox({
-          text("Hello"),
+          text("Applications") | bold | center,
+          vbox(app_elements) | vscroll_indicator | frame | border,
           });
       });
-  win.Loop(renderer);
+
+  auto container = Container::Vertical({layout, exit_btn});
+  auto app = Renderer(container, [&] {
+    return vbox({
+        layout->Render(),
+        exit_btn->Render(),
+        });
+      });
+  screen.Loop(app);
+
+
   return 0;
 }
