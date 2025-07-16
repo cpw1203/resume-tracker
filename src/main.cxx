a4e9820e2b02c574f13e1352ed8f4b619567d5a2
Christopher Waschenko <cwaschenk03@gmail.com>
2025-07-16 19:18:03 -0400

    edit:

--- src/main.cxx
+++ src/main.cxx
@@ -7,42 +7,42 @@
 
 using namespace ftxui;
 
-int main(int argc, char** argv)
-{
-  auto screen = ScreenInteractive::Fullscreen();
-  auto exit_btn = Button("Exit", [&]{ screen.Exit(); }, ButtonOption::Animated());
-  std::vector<Application> apps;
-  std::vector<Element> app_elements;
-  auto layout = Renderer([&](){
-    for(auto& app : apps){
-      char buffer[80];
-      std::tm* tm_info = std::localtime(&app.date_applied);
-      std::strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", tm_info);
-      app_elements.push_back(
-          flexbox({
-            text(app.title) | bold | center,
-            separatorHSelector(0,5000,Color::Red, Color::White),
-            text(app.company) | bold | color(Color::Blue) | center,
-            separatorHSelector(0,5000,Color::Red, Color::White),
-            text(buffer) | bold,
-            }) | border
-          );
-      }
-      return vbox({
-          text("Applications") | bold | center,
-          vbox(app_elements) | vscroll_indicator | frame | border,
+int main(int argc, char** argv) {
+	auto screen = ScreenInteractive::Fullscreen();
+	std::vector<Application> apps;
+  std::array<bool, 30> states;
+	std::vector<Component> application_components;
+	auto applications_container = Container::Vertical(application_components);
+	auto add_application_btn = Button("Add Application", [&] { 
+			auto new_app = Application("SW Engineer", "SpaceX");
+			apps.push_back(new_app); 
+			auto app_text = Renderer([=]{
+					return vbox({
+							text(new_app.title) | bold, 
+							separatorHSelector(0,5000,Color::Red, Color::White),
+							text(new_app.company) | bold
+							}) | border;
+					});
+      Component app = Container::Horizontal({
+        app_text,
+        Checkbox("", &states[0]),
           });
-      });
+			application_components.push_back(app);	
+			applications_container->Add(app);
+			});
 
-  auto container = Container::Vertical({layout, exit_btn});
-  auto app = Renderer(container, [&] {
-    return vbox({
-        layout->Render(),
-        exit_btn->Render(),
-        });
-      });
-  screen.Loop(app);
+	auto delete_application_btn = Button("Remove Application", [] { std::cout << "Remove Application\n"; });
+	auto exit_btn = Button("Exit", [&screen]{screen.Exit();});
+	auto tool_bar = Container::Horizontal({ add_application_btn, delete_application_btn, exit_btn });
+	auto application_view = Renderer(applications_container, [&] {
+			return vbox({
+					text("Applications") | bold | center,
+					applications_container->Render() | frame | border	
+					});
+			});
+	auto main_layout = Container::Vertical({tool_bar, application_view});
 
+	screen.Loop(main_layout);
 
-  return 0;
+	return 0;
 }
